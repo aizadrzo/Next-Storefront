927452997af27445813b97b59b690049ad6a612d
Aizad Ridzo <aizadridzoo@gmail.com>
2024-01-22 17:40:11 +0800

    create hook for fetching data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,15 @@
 "use client";
-import { useEffect, useState } from "react";
+import useFetchProducts from "@/hooks/useFetchProducts";
 
 export default function Home() {
-  const [product, setProduct] = useState();
-
-  useEffect(() => {
-    const fetchProductData = async () => {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      setProduct(data);
-    };
-
-    fetchProductData();
-  });
+  const { products } = useFetchProducts();
 
   return (
     <main className="flex min-h-screen flex-col p-24">
       <h1>My Storefront</h1>
       <div>
-        {product?.map((product) => (
-          <h1>{product?.title}</h1>
+        {products?.map((product) => (
+          <h1 key={product.id}>{product?.title}</h1>
         ))}
       </div>
     </main>
