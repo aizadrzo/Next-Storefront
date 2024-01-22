836bd4d2ad9891d679567445363a1cf8b61b07c1
Aizad Ridzo <aizadridzoo@gmail.com>
2024-01-22 17:33:07 +0800

    chore: made api calls for fetching product data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [product, setProduct] = useState();
+
+  useEffect(() => {
+    const fetchProductData = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProduct(data);
+    };
+
+    fetchProductData();
+  });
+
   return (
     <main className="flex min-h-screen flex-col p-24">
       <h1>My Storefront</h1>
+      <div>
+        {product?.map((product) => (
+          <h1>{product?.title}</h1>
+        ))}
+      </div>
     </main>
   );
 }
