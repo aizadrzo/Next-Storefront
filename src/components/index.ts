b9d53115ec8564af1dd61d4fd955221375ba7e3d
Aizad Ridzo <aizadridzoo@gmail.com>
2024-01-23 23:02:17 +0800

    added a bunch of stuff

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as ProductCard } from "./product-card";
 export { default as SinglePageProduct } from "./single-page-product";
+export { default as Navbar } from "./navbar";
--- src/components/single-page-product.tsx
+++ src/components/single-page-product.tsx
@@ -11,14 +11,14 @@ const SinglePageProduct = ({
   rating,
 }: SingleProductPageProps) => (
   <div className="grid grid-cols-1 md:grid-cols-2 gap-[60px] items-start">
-    <div className="border border-base-200 grid place-content-center p-10">
-      <img src={image} alt={title} className="w-[300px] h-auto" />
+    <div className="border border-base-300 aspect-square p-10 lg:p-20 flex items-center justify-center">
+      <img src={image} alt={title} className="w-[300px] lg:w-[450px] h-auto" />
     </div>
     <div className="space-y-12">
       <div className="space-y-5">
         <h1 className="uppercase text-xl font-bold">{title}</h1>
         <p>{rating?.count}</p>
-        <h2 className="text-3xl font-semibold">$ {price}</h2>
+        <h2 className="text-2xl font-semibold">$ {price}</h2>
         <p className="text-sm">{description}</p>
       </div>
       <div className="w-full flex flex-col gap-y-3">
