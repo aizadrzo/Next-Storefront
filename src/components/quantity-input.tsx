6c21c8996b799c57d75e7b0c959c09cd7dfe0be6
Aizad Ridzo <aizadridzoo@gmail.com>
2024-01-24 01:56:17 +0800

    chore: add input component

---
+++ src/components/quantity-input.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+type QuantityInputType = {
+  quantity: number;
+  onIncrement: () => void;
+  onDecrement: () => void;
+};
+
+const QuantityInput = ({
+  quantity,
+  onIncrement,
+  onDecrement,
+}: QuantityInputType) => (
+  <div className="grid grid-cols-3 h-[48px] border border-primary">
+    <button
+      type="button"
+      className="leading-10 text-primary transition hover:bg-base-200"
+      onClick={onDecrement}
+    >
+      &minus;
+    </button>
+
+    <input
+      type="number"
+      id="Quantity"
+      value={quantity}
+      className="border-transparent text-center [-moz-appearance:_textfield] sm:text-sm [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+    />
+
+    <button
+      type="button"
+      className="leading-10 text-primary transition hover:bg-base-200"
+      onClick={onIncrement}
+    >
+      {"\u002B"}
+    </button>
+  </div>
+);
+
+export default QuantityInput;
--- src/components/single-page-product.tsx
+++ src/components/single-page-product.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { QuantityInput } from ".";
 import { ProductsType } from "@/types/hooks.types";
 
 type SingleProductPageProps = Partial<Exclude<ProductsType, "category" | "id">>;
@@ -9,26 +11,55 @@ const SinglePageProduct = ({
   image,
   price,
   rating,
-}: SingleProductPageProps) => (
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-[60px] items-start">
-    <div className="border border-base-300 aspect-square p-10 lg:p-20 flex items-center justify-center">
-      <img src={image} alt={title} className="w-[300px] lg:w-[450px] h-auto" />
-    </div>
-    <div className="space-y-12">
-      <div className="space-y-5">
-        <h1 className="uppercase text-xl font-bold">{title}</h1>
-        <p>{rating?.count}</p>
-        <h2 className="text-2xl font-semibold">$ {price}</h2>
-        <p className="text-sm">{description}</p>
+}: SingleProductPageProps) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    if (quantity < 10) {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-[60px] items-start">
+      <div className="border border-base-300 aspect-square p-10 lg:p-20 flex items-center justify-center">
+        <img
+          src={image}
+          alt={title}
+          className="w-[300px] lg:w-[450px] h-auto"
+        />
       </div>
-      <div className="w-full flex flex-col gap-y-3">
-        <button className="w-full btn btn-primary">ADD TO CART</button>
-        <button className="w-full btn btn-outline btn-secondary">
-          BUY NOW
-        </button>
+      <div className="space-y-12">
+        <div className="space-y-5">
+          <h1 className="uppercase text-xl font-bold">{title}</h1>
+          <p>{rating?.count}</p>
+          <h2 className="text-2xl font-semibold">$ {price}</h2>
+          <p className="text-sm">{description}</p>
+        </div>
+        <div className="w-full flex flex-col gap-y-3">
+          <div className="flex gap-x-[10px]">
+            <div className="w-1/3 h-full">
+              <QuantityInput
+                quantity={quantity}
+                onIncrement={handleIncrement}
+                onDecrement={handleDecrement}
+              />
+            </div>
+            <button className="grow btn btn-primary">ADD TO CART</button>
+          </div>
+          <button className="w-full btn btn-outline btn-secondary">
+            BUY NOW
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SinglePageProduct;
