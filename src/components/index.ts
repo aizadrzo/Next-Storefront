1490d9331ac103aaa4460c0417af21fcdb04aae8
Aizad Ridzo <aizadridzoo@gmail.com>
2024-02-09 20:03:31 +0800

    added notification as a component

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export { default as CartItem } from "./cart-item";
 export { default as EmptyCart } from "./empty-cart";
 export { default as Footer } from "./footer";
 export { default as StarRating } from "./star-rating";
+export { default as Notification } from "./notification";
